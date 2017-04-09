c53e8c82547aa95fede6d16c3bb08bbbbc78967a
Martin Frost <=>
2017-04-09 17:59:44 +0200

    Add payment view and redirect POST requests from select_seats to it
    * `select_seats` can now handle POST requests, check session timeouts (ish) and
      create Reservation objects.
    
    * Rename 'enter_contact_details' template to 'payments'

--- karspexet/ticket/urls.py
+++ karspexet/ticket/urls.py
@@ -4,5 +4,6 @@ from karspexet.ticket import views
 
 urlpatterns = [
     url(r"^show/(?P<show_id>\d+)/select_seats$", views.select_seats, name="select_seats"),
+    url(r"^show/(?P<show_id>\d+)/payment$", views.payment, name="payment"),
     url(r"^$", views.home),
 ]
--- karspexet/ticket/views.py
+++ karspexet/ticket/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from dateutil.relativedelta import relativedelta
+from dateutil import parser
+from django.shortcuts import render, redirect
+from django.forms.formsets import formset_factory
+from django.utils import timezone
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 
 from karspexet.show.models import Show
+from karspexet.venue.models import Seat
+from karspexet.ticket.models import Reservation
 from karspexet.ticket.forms import TicketTypeForm, SeatingGroupFormSet
+
+# TODO: Do we need to nuke the session in select_seats? I think it's only necessary in the latter steps.
+# Also, have longer timeout than 15 minutes
+# Then, double check the timeout before asking stripe about the charge.payment
 
 
 def home(request):
@@ -14,7 +26,25 @@ def home(request):
 
 def select_seats(request, show_id):
     show = Show.objects.get(id=show_id)
-    forms = [SeatingGroupFormSet(seating_group) for seating_group in show.venue.seatinggroup_set.all()]
+    reservation = _get_or_create_reservation_object(request, show)
+    taken_seats_qs = Reservation.active.exclude(pk=reservation.pk).filter(show=show)
+    _set_session_timeout(request)
+
+    if request.POST:
+        seat_params = _seat_specifications(request)
+        if _all_seats_available(taken_seats_qs, seat_params.keys()):
+            reservation.tickets = seat_params
+            reservation.save()
+            return redirect("payment", show_id=show_id)
+        else:
+            messages.error(request, "Some of your chosen seats are already taken")
+
+    taken_seats = set(map(int,set().union(*[r.tickets.keys() for r in taken_seats_qs.all()])))
+    forms = [
+        SeatingGroupFormSet(seating_group, taken_seats)
+        for seating_group
+        in show.venue.seatinggroup_set.all()
+    ]
 
     return render(request, "select_seats.html", {
         'show': show,
@@ -22,4 +52,62 @@ def select_seats(request, show_id):
         'forms': forms,
     })
 
-    # return render(request, 'enter_contact_details.html', {})
+def payment(request, show_id):
+    # TODO: verify that reservation_timeout has not passed
+    # TODO: call stripe and verify data
+    # * create account/customer object
+    # * create ticket objects
+    # * mark reservation as paid
+    # * send email to customer
+
+    if _session_expired(request):
+        messages.warning(request, "Your session has expired. Please start over from scratch.")
+        return redirect("select_seats", show_id=show_id)
+
+    _set_session_timeout(request)
+    show = Show.objects.get(pk=show_id)
+    reservation = _get_or_create_reservation_object(request, show)
+    seats = Seat.objects.filter(pk__in=reservation.tickets.keys())
+
+    return render(request, 'payment.html', {
+        'show': show,
+        'seats': seats,
+        'reservation': reservation,
+    })
+
+
+def _session_expired(request):
+    timeout = request.session.get('reservation_timeout', None)
+    if timeout:
+        timeout = parser.parse(timeout)
+        return timeout < timezone.now()
+
+def _set_session_timeout(request):
+    request.session['reservation_timeout'] = (timezone.now() + relativedelta(minutes=15)).isoformat()
+
+def _get_or_create_reservation_object(request, show):
+    timeout = timezone.now() + relativedelta(minutes=15)
+
+    if request.session.get('reservation_id'):
+        try:
+            reservation = Reservation.objects.get(pk=request.session['reservation_id'], show=show)
+            reservation.session_timeout = timeout
+            reservation.save()
+            return reservation
+        except ObjectDoesNotExist:
+            pass
+
+    reservation = Reservation.objects.create(show=show, total=0, tickets={}, session_timeout=timeout)
+    request.session['reservation_id'] = reservation.id
+
+    return reservation
+
+def _all_seats_available(qs, seat_ids):
+    return not qs.filter(tickets__has_any_keys=seat_ids).exists()
+
+def _seat_specifications(request):
+    return {
+        int(seat.replace("seat_", "")):ticket_type
+            for seat,ticket_type in request.POST.items()
+            if seat.startswith("seat_")
+    }
