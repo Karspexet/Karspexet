7cdc04dafc5a4cd7b37325976b183e6859677960
Martin Frost <=>
2017-04-09 17:56:15 +0200

    Introduce Account model and flesh out Ticket
    Also add manager for Reservation.active

--- karspexet/ticket/models.py
+++ karspexet/ticket/models.py
@@ -1,18 +1,43 @@
 from django.conf import settings
 from django.contrib.postgres.fields import HStoreField
 from django.db import models
+from django.db.models import Q
+from django.utils import timezone
+
+from karspexet.show.models import Show
+from karspexet.venue.models import Seat
 
 
 # TODO: These should probably all be timestamped
 
 
+class ActiveReservationsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(Q(session_timeout__gt=timezone.now()) | Q(finalized=True))
+
+
 class Reservation(models.Model):
+    show = models.ForeignKey(Show, null=False)
     total = models.IntegerField()
     tickets = HStoreField()
+    session_timeout = models.DateTimeField()
+    finalized = models.BooleanField(default=False)
+
+    objects = models.Manager()
+    active = ActiveReservationsManager()
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
 
 
 class Ticket(models.Model):
     price = models.IntegerField()
+    ticket_type = models.CharField(max_length=10) # TODO: Validate that ticket type is in ["normal", "student"]
+    show = models.ForeignKey(Show, null=False)
+    seat = models.ForeignKey(Seat, null=False)
+    account = models.ForeignKey(Account, null=False)
 
 
 class Voucher(models.Model):
