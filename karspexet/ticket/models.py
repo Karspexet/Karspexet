cff852ebdf1df7c32a838ceaf2ee94c3bc178647
Martin Frost <=>
2017-04-27 08:18:06 +0200

    Timestamp remaining ticket models

--- karspexet/ticket/models.py
+++ karspexet/ticket/models.py
@@ -8,9 +8,6 @@ from karspexet.show.models import Show
 from karspexet.venue.models import Seat
 
 
-# TODO: These should probably all be timestamped
-
-
 class ActiveReservationsManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(Q(session_timeout__gt=timezone.now()) | Q(finalized=True))
@@ -32,6 +29,8 @@ class Reservation(models.Model):
 class Account(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
 
 
 class Ticket(models.Model):
@@ -40,9 +39,13 @@ class Ticket(models.Model):
     show = models.ForeignKey(Show, null=False)
     seat = models.ForeignKey(Seat, null=False)
     account = models.ForeignKey(Account, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
 
 
 class Voucher(models.Model):
     reservation = models.ForeignKey(Reservation, null=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
     rebate_amount = models.IntegerField(help_text="Rabatt i SEK")
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
