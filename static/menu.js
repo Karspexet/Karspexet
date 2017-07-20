57027a488c55b33b07cbbad23c0dd52f65c38280
David Tennander <david.tennander@gmail.com>
2017-07-20 22:38:20 +0200

    Removing `addtoblock` parts in base.html. And moving from the lamnda expression `() => {}`.

--- static/menu.js
+++ static/menu.js
@@ -2,9 +2,9 @@ const navigationBar = $("#navbar");
 
 $("#contentBox").css("top", $("#navbar").css("height")); //Sets the content below the navbar.
 
-document.onscroll = () => {
-    const titleHeight = parseInt($(".titleContainer").css("height"));
-    const navbarHeigt = parseInt($("#navbar").css("height"));
+document.onscroll = function () {
+    const titleHeight = parseInt($(".titleContainer").css("height"), 10);
+    const navbarHeigt = parseInt($("#navbar").css("height"), 10);
     const contentBx = $("#contentBox");
     if (titleHeight > window.pageYOffset){
         navigationBar.css("top", 0);
