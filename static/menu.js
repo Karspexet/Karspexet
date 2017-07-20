ab233c06a12337a9b31e9b91cba1228cedfcbf0a
David Tennander <david.tennander@gmail.com>
2017-07-20 18:20:31 +0200

    Moving down the content text to be below the navbar.

--- static/menu.js
+++ static/menu.js
@@ -1,12 +1,18 @@
 const navigationBar = $("#navbar");
 
+$("#contentBox").css("top", $("#navbar").css("height")); //Sets the content below the navbar.
+
 document.onscroll = () => {
     const titleHeight = parseInt($(".titleContainer").css("height"));
-
+    const navbarHeigt = parseInt($("#navbar").css("height"));
+    const contentBx = $("#contentBox");
     if (titleHeight > window.pageYOffset){
-        navigationBar.css("top", -window.pageYOffset);
+        navigationBar.css("top", 0);
+        navigationBar.css("position", "absolute");
     } else {
         navigationBar.css("top", -titleHeight);
+        navigationBar.css("position", "fixed");
     }
+    contentBx.css("top", navbarHeigt);
 
 }
