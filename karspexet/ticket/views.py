f402b3ecb315ebfbbd2b52a4e3a36122ab1dd8ed
Martin Frost <frost@ceri.se>
2017-10-10 08:30:34 +0200

    Add option for whether a show should be visible on the front page
    This can be used for testing purposes or for shows that are only visible to
    people with the right link.
    
    This commit also rewrites some of the tests for the ticket views to assert
    things on TemplateResponse objects instead of doing string comparisons on the
    rendered HTML.

--- karspexet/ticket/views.py
+++ karspexet/ticket/views.py
@@ -7,7 +7,8 @@ from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.template.response import TemplateResponse
 from django.utils import timezone
 
 from karspexet.show.models import Show
@@ -25,7 +26,7 @@ SESSION_TIMEOUT_MINUTES = 30
 
 
 def home(request):
-    upcoming_shows = Show.upcoming()
+    upcoming_shows = Show.upcoming().filter(visible=True).all()
 
     return render(request, "ticket/ticket.html", {
         'upcoming_shows': upcoming_shows
@@ -61,6 +62,7 @@ def select_seats(request, show_id):
         'seats': json.dumps(seats)
     })
 
+
 def booking_overview(request):
     reservation = _get_or_create_reservation_object(request)
 
@@ -80,6 +82,7 @@ def booking_overview(request):
         'reservation': reservation,
         'stripe_key': stripe_keys['publishable_key'],
     })
+
 
 def process_payment(request, reservation_id):
     reservation = Reservation.objects.get(pk=reservation_id)
@@ -113,8 +116,10 @@ def _session_expired(request):
         timeout = parser.parse(timeout)
         return timeout < timezone.now()
 
+
 def _set_session_timeout(request):
     request.session['reservation_timeout'] = (timezone.now() + relativedelta(minutes=SESSION_TIMEOUT_MINUTES)).isoformat()
+
 
 def _get_or_create_reservation_object(request, show=None):
     timeout = timezone.now() + relativedelta(minutes=SESSION_TIMEOUT_MINUTES)
@@ -133,8 +138,10 @@ def _get_or_create_reservation_object(request, show=None):
 
     return reservation
 
+
 def _all_seats_available(qs, seat_ids):
     return not qs.filter(tickets__has_any_keys=seat_ids).exists()
+
 
 def _seat_specifications(request):
     return {
@@ -142,6 +149,7 @@ def _seat_specifications(request):
             for seat,ticket_type in request.POST.items()
             if seat.startswith("seat_")
     }
+
 
 def _some_seat_is_missing_ticket_type(seat_params):
     return any(not ticket_type for ( seat,ticket_type ) in seat_params.items())
@@ -160,6 +168,7 @@ def _build_pricings_and_seats(venue):
     }
 
     return (pricings, seats)
+
 
 def _payment_partial():
     if settings.PAYMENT_PROCESS == "stripe":
