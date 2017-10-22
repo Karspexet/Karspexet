862029ebfdeb9043f5d12eaea042ec7646126157
Martin Frost <frost@ceri.se>
2017-10-22 21:31:56 +0200

    Tidy up economy view tests a bit

--- karspexet/economy/tests/test_views.py
+++ karspexet/economy/tests/test_views.py
@@ -9,23 +9,24 @@ from factories import factories as f
 from karspexet.economy import views
 
 class TestOverview(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_only_staff_can_access_overview_page(self):
-        client = Client()
-        response = client.get("/economy/")
+        response = self.client.get("/economy/")
         assert response.status_code == 302
 
-        user = User.objects.create_user("ture", email="ture@example.com")
-        user.set_password("test")
-        user.is_staff = True
-        user.save()
+        _create_staff_user("ture", "test")
+        assert self.client.login(username="ture", password="test")
 
-        assert client.login(username="ture", password="test")
-
-        response = client.get("/economy/")
+        response = self.client.get("/economy/")
         assert "Föreställningsöversikt" in response.content.decode("utf-8")
 
 
 class TestShowDetail(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_only_staff_can_access_detail_page(self):
         production = f.CreateProduction()
         venue = f.CreateVenue()
@@ -33,16 +34,18 @@ class TestShowDetail(TestCase):
         seat = f.CreateSeat(group=seating_group)
         show = f.CreateShow(date=timezone.now(), production=production, venue=venue)
 
-        client = Client()
-        response = client.get(f"/economy/{show.id}")
+        response = self.client.get(f"/economy/{show.id}")
         assert response.status_code == 302
 
-        user = User.objects.create_user("ture", email="ture@example.com")
-        user.set_password("test")
-        user.is_staff = True
-        user.save()
+        _create_staff_user("ture", "test")
+        assert self.client.login(username="ture", password="test")
 
-        assert client.login(username="ture", password="test")
-
-        response = client.get(f"/economy/{show.id}")
+        response = self.client.get(f"/economy/{show.id}")
         assert "Föreställningsöversikt" in response.content.decode("utf-8")
+
+def _create_staff_user(username, password):
+    user = User.objects.create_user(username, email="ture@example.com")
+    user.set_password(password)
+    user.is_staff = True
+    user.save()
+    return user
