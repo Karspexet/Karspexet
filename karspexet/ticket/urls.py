187622c35a089ad62f938899483fa8a0cef83574
Martin Frost <frost@ceri.se>
2017-11-21 21:01:59 +0100

    Add ticket detail page
    A successful payment will redirect to the reservation overview page.
    
    This page will be available at `/reservation/<reservation_code>` and will be
    used by the customer to access the reservation.

--- karspexet/ticket/urls.py
+++ karspexet/ticket/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r"^show/(?P<show_id>\d+)/select_seats$", views.select_seats, name="select_seats"),
     url(r"^reservation/(?P<reservation_id>\d+)/process_payment$", views.process_payment, name="process_payment"),
     url(r"^booking_overview/?$", views.booking_overview, name="booking_overview"),
+    url(r"^reservation/(?P<reservation_code>[A-Z0-9]+)/?$", views.reservation_detail, name="reservation_detail"),
     url(r"^$", views.home),
 ]
