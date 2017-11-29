eb35d6f08d9e1ebddf749eeb0ac99ab795e934be
Martin Frost <frost@ceri.se>
2017-11-29 23:27:01 +0100

    Add Django CMS
    ...and a bunch of plugins to make it work.
    
    This will be used for building the new version of karspexet.se.

--- karspexet/urls.py
+++ karspexet/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r"^admin/", include(admin.site.urls)),
     url(r"^economy/", include("karspexet.economy.urls")),
-    url(r"^", include("karspexet.ticket.urls"))
+    url(r"^ticket/", include("karspexet.ticket.urls")),
+    url(r"^", include("cms.urls")),
 ]
