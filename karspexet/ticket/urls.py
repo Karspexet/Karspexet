14803816c50bc1557c633f5d11f2c7a6d339429a
Martin Frost <frost@ceri.se>
2017-12-06 08:46:35 +0100

    Allow trailing slash on select_seats url

--- karspexet/ticket/urls.py
+++ karspexet/ticket/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from karspexet.ticket import views
 
 urlpatterns = [
-    url(r"^show/(?P<show_id>\d+)/select_seats$", views.select_seats, name="select_seats"),
+    url(r"^show/(?P<show_id>\d+)/select_seats/?$", views.select_seats, name="select_seats"),
     url(r"^reservation/(?P<reservation_id>\d+)/process_payment$", views.process_payment, name="process_payment"),
     url(r"^booking_overview/?$", views.booking_overview, name="booking_overview"),
     url(r"^reservation/(?P<reservation_code>[A-Z0-9]+)/?$", views.reservation_detail, name="reservation_detail"),
