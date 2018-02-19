d7a1f144221dd0e8d9df452b2d787649efaaa913
Martin Frost <frost@ceri.se>
2018-02-19 20:39:30 +0100

    Add CreateStaffUser test factory

--- factories/factories.py
+++ factories/factories.py
@@ -1,12 +1,16 @@
 from factory.django import DjangoModelFactory
+from django.contrib.auth.models import User
+
 
 class CreateVenue(DjangoModelFactory):
     class Meta:
         model = 'venue.venue'
 
+
 class CreateSeatingGroup(DjangoModelFactory):
     class Meta:
         model = 'venue.seatinggroup'
+
 
 class CreateSeat(DjangoModelFactory):
     x_pos = 0
@@ -15,30 +19,45 @@ class CreateSeat(DjangoModelFactory):
     class Meta:
         model = 'venue.seat'
 
+
 class CreateShow(DjangoModelFactory):
     class Meta:
         model = 'show.show'
+
 
 class CreateProduction(DjangoModelFactory):
     class Meta:
         model = 'show.production'
 
+
 class CreateReservation(DjangoModelFactory):
     class Meta:
         model = 'ticket.reservation'
+
 
 class CreateAccount(DjangoModelFactory):
     class Meta:
         model = 'ticket.account'
 
+
 class CreateTicket(DjangoModelFactory):
     class Meta:
         model = 'ticket.ticket'
+
 
 class CreateVoucher(DjangoModelFactory):
     class Meta:
         model = 'ticket.voucher'
 
+
 class CreatePricingModel(DjangoModelFactory):
     class Meta:
         model = 'ticket.pricingmodel'
+
+
+def CreateStaffUser(username, password):
+    user = User.objects.create_user(username, email="ture@example.com")
+    user.set_password(password)
+    user.is_staff = True
+    user.save()
+    return user
