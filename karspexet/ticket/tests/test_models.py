934d3e9b0d0871829a10f73d719423fca5c1da5b
Martin Frost <frost@ceri.se>
2018-02-19 20:40:09 +0100

    Add tests for Voucher and Discount models
    Also, clean up the test code a bit, so that reservation initialization does not
    have to provide a `total` field.

--- karspexet/ticket/tests/test_models.py
+++ karspexet/ticket/tests/test_models.py
@@ -1,13 +1,18 @@
 import pytest
 
+from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 from django.test import TestCase
 from django.utils import timezone
 
+from unittest.mock import patch
+
 from factories import factories as f
 
-from karspexet.ticket.models import PricingModel, Reservation, Ticket
+from karspexet.ticket.models import PricingModel, Reservation, Ticket, Voucher, Discount, AlreadyDiscountedException, InvalidVoucherException
+from karspexet.venue.models import Seat
 from django.core.exceptions import ValidationError
+
 
 class TestReservation(TestCase):
     def setUp(self):
@@ -30,7 +35,6 @@ class TestReservation(TestCase):
             show=self.show,
             session_timeout=timestamp,
             tickets={},
-            total=200,
         )
 
         assert reservation in Reservation.active.all()
@@ -42,7 +46,6 @@ class TestReservation(TestCase):
             finalized=True,
             session_timeout=timestamp,
             tickets={},
-            total=200,
         )
 
         assert reservation in Reservation.active.all()
@@ -53,27 +56,24 @@ class TestReservation(TestCase):
             show=self.show,
             session_timeout=timestamp,
             tickets={},
-            total=200,
         )
 
         assert reservation in Reservation.objects.all()
         assert reservation not in Reservation.active.all()
 
 
-    def test_total_price(self):
+    def test_ticket_price_and_total(self):
         timestamp = timezone.now() - relativedelta(days=1)
         reservation = Reservation.objects.create(
             show=self.show,
-            total=0,
             session_timeout=timestamp,
             tickets={
-                self.seat1.id: 'student',
-                self.seat2.id: 'normal',
+                str(self.seat1.id): 'student',
+                str(self.seat2.id): 'normal',
             },
         )
-        reservation = Reservation.objects.get(pk=reservation.id)
 
-        assert reservation.total_price() == 450
+        assert reservation.total == 450
 
     def test_reservations_have_auto_generated_reservation_code(self):
         reservation_1 = Reservation()
@@ -146,6 +146,109 @@ class TestTicket(TestCase):
             duplicate_ticket.full_clean()
 
 
+@pytest.fixture
+def show():
+    venue = f.CreateVenue()
+    group = f.CreateSeatingGroup(venue=venue)
+    pricing = f.CreatePricingModel(
+        seating_group=group,
+        prices={'student': 200, 'normal': 250},
+        valid_from=timezone.now()
+    )
+    f.CreateSeat(group=group)
+    f.CreateSeat(group=group)
+
+    production = f.CreateProduction()
+    return f.CreateShow(production=production, venue=venue, date=timezone.now())
+
+
+@pytest.fixture
+def user():
+    return f.CreateStaffUser(username="test", password="test")
+
+
+@pytest.mark.django_db
 class TestDiscount:
-    def discount_must_have_a_unique_voucher_reservation_combo():
-        pass
+    def test_discount_lowers_the_total_of_a_reservation(self, show, user):
+        seat = Seat.objects.first()
+        tickets = {str(seat.id): 'normal'}
+        reservation = f.CreateReservation(tickets=tickets, session_timeout=timezone.now(), show=show)
+        voucher = Voucher.objects.create(amount=100, created_by=user)
+        discount = reservation.apply_voucher(voucher.code)
+
+        assert discount.amount == 100
+        assert reservation.total == reservation.ticket_price - 100
+
+    def test_discount_can_result_in_free_tickets(self, show, user):
+        seat = Seat.objects.first()
+        tickets = {str(seat.id): 'normal'}
+        reservation = f.CreateReservation(tickets=tickets, session_timeout=timezone.now(), show=show)
+        voucher = Voucher.objects.create(amount=reservation.ticket_price, created_by=user)
+        discount = reservation.apply_voucher(voucher.code)
+
+        assert discount.amount == reservation.ticket_price
+        assert reservation.total == 0
+
+    def test_excess_voucher_amount_is_void(self, show, user):
+        seat = Seat.objects.first()
+        tickets = {str(seat.id): 'normal'}
+        reservation = f.CreateReservation(tickets=tickets, session_timeout=timezone.now(), show=show)
+        voucher = Voucher.objects.create(amount=300, created_by=user)
+        discount = reservation.apply_voucher(voucher.code)
+
+        assert discount.amount == reservation.ticket_price
+        assert reservation.total == 0
+
+    def test_a_voucher_cannot_be_reused(self, show, user):
+        seat = Seat.objects.all()[0]
+        seat2 = Seat.objects.all()[1]
+        reservation = f.CreateReservation(
+            tickets={str(seat.id): 'normal'},
+            session_timeout=timezone.now(),
+            show=show
+        )
+        reservation2 = f.CreateReservation(
+            tickets={str(seat.id): 'normal'},
+            session_timeout=timezone.now(),
+            show=show
+        )
+
+        voucher = Voucher.objects.create(amount=300, created_by=user)
+
+        discount = reservation.apply_voucher(voucher.code)
+        with pytest.raises(InvalidVoucherException):
+            reservation2.apply_voucher(voucher.code)
+
+        assert discount.amount == reservation.ticket_price
+        assert reservation.total == 0
+        assert reservation2.total == reservation2.ticket_price
+
+    def test_a_voucher_cannot_have_multiple_discounts(self, show, user):
+        seat = Seat.objects.first()
+        tickets = {str(seat.id): 'normal'}
+        reservation = f.CreateReservation(tickets=tickets, session_timeout=timezone.now(), show=show)
+        voucher = Voucher.objects.create(amount=100, created_by=user)
+        voucher2 = Voucher.objects.create(amount=100, created_by=user)
+        discount = reservation.apply_voucher(voucher.code)
+
+        with pytest.raises(AlreadyDiscountedException):
+            reservation.apply_voucher(voucher2.code)
+
+        assert discount.amount == 100
+        assert reservation.total == 150
+
+
+    def test_a_voucher_expires_nextcoming_fifteenth_of_september(self):
+        user = f.CreateStaffUser(username="test", password="test")
+        this_year = date.today().year
+        next_year = this_year + 1
+        start_of_year = datetime(this_year, 1, 2)
+        end_of_year = datetime(this_year, 12, 30)
+        with patch("karspexet.ticket.models.timezone.now", return_value=start_of_year):
+            voucher = Voucher(amount=100, created_by=user)
+            assert voucher.expiry_date == date(this_year, 9, 15)
+
+        with patch("karspexet.ticket.models.timezone.now", return_value=end_of_year):
+            voucher = Voucher(amount=100, created_by=user)
+            assert voucher.expiry_date == date(next_year, 9, 15)
+
