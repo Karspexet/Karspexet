69f33e92c071d82701a6e68de66f6466b771bc88
Dan Strokirk <dan.strokirk@gmail.com>
2018-04-08 01:09:25 +0200

    Update admin site urls to latest Django standard
    See: https://docs.djangoproject.com/en/1.10/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf

--- karspexet/urls.py
+++ karspexet/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r"^admin/", include(admin.site.urls)),
+    url(r"^admin/", admin.site.urls),
     url(r"^economy/", include("karspexet.economy.urls")),
     url(r"^ticket/", include("karspexet.ticket.urls")),
     url(r"^", include("cms.urls")),
