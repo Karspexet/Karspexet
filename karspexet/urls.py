fd30d0e3ed43a1df6f63f7800d4d687941aabcbc
Dan Strokirk <dan.strokirk@gmail.com>
2019-12-08 20:32:13 +0100

    Add a styled 500 page

--- karspexet/urls.py
+++ karspexet/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^economy/", include("karspexet.economy.urls")),
     url(r"^ticket/", include("karspexet.ticket.urls")),
+
+    url(r"^500/", TemplateView.as_view(template_name="500.html")),
+
     url(r"^", include("cms.urls")),
 ]
