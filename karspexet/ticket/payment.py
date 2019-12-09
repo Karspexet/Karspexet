70ed718150def643951385498b837ec5dcd31829
Dan Strokirk <dan.strokirk@gmail.com>
2019-12-09 13:30:05 +0100

    Handle when we already created an Account

--- karspexet/ticket/payment.py
+++ karspexet/ticket/payment.py
@@ -75,7 +75,9 @@ def handle_successful_payment(reservation: Reservation, billing_data: dict):
         return
 
     billing = _pick(billing_data, ["name", "phone", "email"])
-    account, _ = Account.objects.get_or_create(**billing)
+    account = Account.objects.filter(**billing).first()
+    if account is None:
+        Account.objects.create(**billing)
 
     for seat_id, ticket_type in reservation.tickets.items():
         seat = Seat.objects.get(pk=seat_id)
