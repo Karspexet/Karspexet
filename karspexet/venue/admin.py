bb614ac057d25e9e2b33907e10a618712b973eca
Dan Strokirk <dan.strokirk@gmail.com>
2020-01-01 20:41:05 +0100

    Improve build_seats command to more efficiently load seatmaps

--- karspexet/venue/admin.py
+++ karspexet/venue/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.options import IncorrectLookupParameters
 from django.shortcuts import reverse
 from django.utils.html import format_html
 
@@ -6,37 +7,56 @@ from karspexet.venue.models import Seat, SeatingGroup, Venue
 
 
 def _admin_change_link(obj):
-    return reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[obj.id])
+    return reverse("admin:%s_%s_change" % (obj._meta.app_label, obj._meta.model_name), args=[obj.id])
 
 
-class SeatingGroupInline(admin.StackedInline):
+class VenueFilter(admin.SimpleListFilter):
+    title = "Teater"
+    parameter_name = "venue"
+
+    def lookups(self, request, model_admin):
+        return [(venue.id, venue.name) for venue in Venue.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            try:
+                queryset = queryset.filter(group__venue_id=self.value())
+            except ValueError as e:
+                raise IncorrectLookupParameters(e)
+        return queryset
+
+
+class SeatingGroupInline(admin.TabularInline):
     model = SeatingGroup
-    extra = 1
-    readonly_fields = ['admin_link']
+    extra = 0
+    readonly_fields = ["admin_link"]
 
     @classmethod
     def admin_link(cls, obj):
+        if not obj.pk:
+            return ""
         url = _admin_change_link(obj)
         return format_html(u'<a href="{}">Edit: {}</a>', url, obj)
 
 
-class SeatInline(admin.StackedInline):
+class SeatInline(admin.TabularInline):
     model = Seat
-    extra = 1
+    extra = 0
 
 
+@admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
     inlines = [SeatingGroupInline]
 
 
+@admin.register(SeatingGroup)
 class SeatingGroupAdmin(admin.ModelAdmin):
     inlines = [SeatInline]
+    raw_id_fields = ("venue",)
 
 
+@admin.register(Seat)
 class SeatAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Venue, VenueAdmin)
-admin.site.register(Seat, SeatAdmin)
-admin.site.register(SeatingGroup, SeatingGroupAdmin)
+    list_display = ("name", "group")
+    raw_id_fields = ("group",)
+    list_filter = [VenueFilter]
--- karspexet/venue/management/commands/build_seats.py
+++ karspexet/venue/management/commands/build_seats.py
@@ -9,26 +9,41 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("venue-id", type=int)
         parser.add_argument("file")
+        parser.add_argument("--clear", action="store_true")
 
     def handle(self, *args, **options):
         venue = Venue.objects.get(pk=options["venue-id"])
 
-        with open(options["file"]) as csv_file:
-            reader = csv.reader(csv_file, delimiter="\t")
-            groupings = {}
+        if options["clear"]:
+            Seat.objects.filter(group__venue=venue).delete()
+            SeatingGroup.objects.filter(venue=venue).delete()
 
-            for group_name, seat_name, x_pos, y_pos in reader:
-                group = groupings.get(group_name, None)
-                if group is None:
-                    group = SeatingGroup.objects.create(
-                        name=group_name,
-                        venue=venue
-                    )
-                    groupings[group_name] = group
+        groupings = {group.name: group for group in SeatingGroup.objects.filter(venue=venue)}
 
-                Seat.objects.create(
-                    group=group,
-                    name=seat_name,
-                    x_pos=x_pos,
-                    y_pos=y_pos,
+        count = 0
+        for group_name, x_pos, y_pos, seat_name in self._read_file(options["file"]):
+            group = groupings.get(group_name, None)
+            if group is None:
+                group = SeatingGroup.objects.create(
+                    name=group_name,
+                    venue=venue
                 )
+                groupings[group_name] = group
+
+            _, created = Seat.objects.get_or_create(
+                group=group,
+                name=seat_name,
+                x_pos=x_pos,
+                y_pos=y_pos,
+            )
+            if created:
+                count += created
+
+        total = Seat.objects.filter(group__venue=venue).count()
+        self.stdout.write("Created %s new Seats for %s (now has %s)" % (count, venue, total))
+
+    @staticmethod
+    def _read_file(filename):
+        with open(filename) as csv_file:
+            reader = csv.reader(csv_file)
+            return list(reader)
