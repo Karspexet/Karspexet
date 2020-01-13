73fb4126d04bfef94670dc77c00674caaf68e1b3
David Tennander <david.tennander@gmail.com>
2020-01-13 22:25:23 +0100

    Makes ticket reference non-nullable but default to empty.

--- karspexet/ticket/migrations/0018_ticket_reference.py
+++ karspexet/ticket/migrations/0018_ticket_reference.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='ticket',
             name='reference',
-            field=models.CharField(max_length=255, null=True),
+            field=models.CharField(blank=True, default='', max_length=255),
         ),
     ]
