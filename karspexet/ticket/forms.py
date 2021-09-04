d438b1b51b174a39b3f7fece71a968c32d18f2ae
Dan Strokirk <dan.strokirk@gmail.com>
2021-09-04 15:45:11 +0200

    Fix styling of reservation details
    Tailwind didn't pick up on the code in `karspexet/ticket/`, so let's
    move the templates to the general templates directory.

--- karspexet/ticket/forms.py
+++ karspexet/ticket/forms.py
@@ -5,8 +5,5 @@ class CustomerEmailForm(forms.Form):
     email = forms.EmailField(
         required=True,
         label="E-postadress",
-        widget=forms.TextInput(attrs={
-            "placeholder": "E-postadress",
-            "class": "p-2 w-full box-border",
-        }),
+        widget=forms.TextInput(attrs={"placeholder": "E-postadress"}),
     )
