44f98d20875571fba7277996b69675a0e4d159e2
Dan Strokirk <dan.strokirk@gmail.com>
2021-09-04 15:34:49 +0200

    Add logging to confirmation email sending

--- karspexet/ticket/tasks.py
+++ karspexet/ticket/tasks.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__file__)
 
 
 def send_ticket_email_to_customer(reservation, email, name=None):
-    '''Send an email to the customer with a link to their tickets
+    """Send an email to the customer with a link to their tickets
 
     If the supplied email is empty, this will silently fail. The reason for this is that this is used in the payment
     flow, and if we raise an error here, it will crash the payment transaction, and at that point we have likely
@@ -18,20 +18,20 @@ def send_ticket_email_to_customer(reservation, email, name=None):
     Therefore the trade-off is made that if the customer fails to provide a valid email address, they will not receive
     an email. They will however, have another chance to send the reservation information via email at the
     reservation-detail page.
-    '''
+    """
     if not email:
         return
 
     if not name:
         name = email
 
-    to_address = f'{name} <{email}>'
-    subject = 'Dina biljetter till Kårspexet'
+    to_address = f"{name} <{email}>"
+    subject = "Dina biljetter till Kårspexet"
     site = Site.objects.get_current()
-    reservation_url = f'https://{site.domain}{reservation.get_absolute_url()}'
-    body = render_to_string('reservation_email.txt', {
-        'reservation': reservation,
-        'url': reservation_url,
+    reservation_url = f"https://{site.domain}{reservation.get_absolute_url()}"
+    body = render_to_string("reservation_email.txt", {
+        "reservation": reservation,
+        "url": reservation_url,
     })
 
     try:
@@ -42,5 +42,6 @@ def send_ticket_email_to_customer(reservation, email, name=None):
             [to_address],
             fail_silently=False,
         )
+        logger.info("Send confirmation email to %s", to_address)
     except Exception as e:
         logger.exception("Failed to send Email to %s", to_address)
