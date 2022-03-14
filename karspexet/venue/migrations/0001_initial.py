6bc84b16e5ec3e785514bcd5129557a8d1c37757
Dan Strokirk <dan.strokirk@gmail.com>
2022-03-14 22:00:11 +0100

    Fix mypy warnings

--- karspexet/venue/migrations/0001_initial.py
+++ karspexet/venue/migrations/0001_initial.py
@@ -5,11 +5,8 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
-
-    dependencies = [
-    ]
+    dependencies: list = []
 
     operations = [
         migrations.CreateModel(
