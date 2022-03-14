38b6448f00e5aeac28d64a8a69aacdb1cdc9440a
Dan Strokirk <dan.strokirk@gmail.com>
2022-03-14 23:08:33 +0100

    Improve economy views
    * Create voucher via proper form
    * Format files
    * Sort vouchers in chronological order

--- karspexet/economy/tests/test_views.py
+++ karspexet/economy/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import Client, TestCase
-from django.utils import timezone
+from django.urls import reverse
 
 from factories import factories as f
 
@@ -9,13 +9,14 @@ class TestOverview(TestCase):
         self.client = Client()
 
     def test_only_staff_can_access_overview_page(self):
-        response = self.client.get("/economy/")
+        url = reverse("economy_overview")
+        response = self.client.get(url)
         assert response.status_code == 302
 
         f.CreateStaffUser(username="ture", password="test")
         assert self.client.login(username="ture", password="test")
 
-        response = self.client.get("/economy/")
+        response = self.client.get(url)
         assert "Föreställningsöversikt" in response.content.decode("utf-8")
 
 
@@ -24,17 +25,49 @@ class TestShowDetail(TestCase):
         self.client = Client()
 
     def test_only_staff_can_access_detail_page(self):
-        production = f.CreateProduction()
-        venue = f.CreateVenue()
-        seating_group = f.CreateSeatingGroup(venue=venue)
-        seat = f.CreateSeat(group=seating_group)
-        show = f.CreateShow(date=timezone.now(), production=production, venue=venue)
-        staff = f.CreateStaffUser(username="ture", password="test")
+        venue = f.CreateVenue(num_seats=1)
+        seat = venue.seatinggroup_set.first().seat_set.first()
+        show = f.CreateShow(venue=venue)
+        f.CreateTicket(show=show, seat=seat)
 
-        response = self.client.get(f"/economy/{show.id}/")
+        url = reverse("economy_show_detail", kwargs={"show_id": show.id})
+        response = self.client.get(url)
         assert response.status_code == 302
 
+        staff = f.CreateStaffUser(username="ture", password="test")
         self.client.force_login(staff)
 
-        response = self.client.get(f"/economy/{show.id}/")
+        response = self.client.get(url)
         self.assertContains(response, "Föreställningsöversikt")
+
+
+class TestVouchers(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.staff = f.CreateStaffUser(username="ture", password="test")
+
+    def test_vouchers_list(self):
+        url = reverse("economy_vouchers")
+        assert self.client.get(url).status_code == 302
+
+        self.client.force_login(self.staff)
+
+        response = self.client.post(url, data={"note": "Rabbatkod ABC123", "amount": 100})
+        assert response.status_code == 302
+
+        response = self.client.get(url)
+        self.assertContains(response, "Rabbatkod ABC123")
+
+    def test_discounts_list(self):
+        url = reverse("economy_discounts")
+        assert self.client.get(url).status_code == 302
+
+        self.client.force_login(self.staff)
+
+        venue = f.CreateVenue(num_seats=1)
+        seat = venue.seatinggroup_set.first().seat_set.first()
+        reservation = f.CreateReservation(show__venue=venue, tickets={str(seat.id): "normal"})
+        f.CreateDiscount(reservation=reservation, voucher__code="Rabbat")
+
+        response = self.client.get(url)
+        self.assertContains(response, "Rabbat")
--- karspexet/economy/urls.py
+++ karspexet/economy/urls.py
@@ -5,7 +5,6 @@ from karspexet.economy import views
 urlpatterns = [
     path("", views.overview, name="economy_overview"),
     path("<int:show_id>/", views.show_detail, name="economy_show_detail"),
-    path("create_voucher/", views.create_voucher, name="economy_create_voucher"),
     path("discounts/", views.discounts, name="economy_discounts"),
     path("vouchers/", views.vouchers, name="economy_vouchers"),
 ]
