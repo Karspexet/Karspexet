1fec00778abe9327ae542694de406c58027db084
Dan Strokirk <dan.strokirk@gmail.com>
2022-05-18 20:27:55 +0200

    Add sentry error tracking to frontend

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,15 +1,43 @@
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
 import { setupBookingOverview } from "./booking";
+import "./number-input";
 import { setupPayment } from "./payment";
 import { setupSelectSeats } from "./seats";
-import "./number-input";
 
 declare global {
   interface Window {
-    config: any;
+    config: {
+      clientSecret: string;
+      seatSelection: any;
+      stripeKey: string;
+      successUrl: string;
+    };
   }
 }
 
+function initSentry() {
+  // @ts-expect-error
+  if (import.meta.env.DEV) {
+    return;
+  }
+
+  Sentry.init({
+    dsn: "https://a2c447d27d42448e804317bef19ad89c@o991062.ingest.sentry.io/6418837",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    tracesSampleRate: 0.05,
+  });
+
+  // @ts-ignore
+  window.triggerError = () => console.log(myUndefinedFunction());
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  initSentry();
+
   const { config } = window;
   if (config) {
     setupSelectSeats(config);
