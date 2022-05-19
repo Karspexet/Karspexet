8f4fd8ee6f6f4d75b4424784df4b915113454a88
Dan Strokirk <dan.strokirk@gmail.com>
2022-05-19 21:38:38 +0200

    Fix issues when session timeout reaches bottom

--- assets/js/booking.ts
+++ assets/js/booking.ts
@@ -1,16 +1,22 @@
 import $ from "cash-dom";
 
 export function setupBookingOverview() {
-  $(".fn-session-timeout").each((i, element) => {
-    let timeElement = $("time");
-    let outputElement = $(".fn-session-timeout-output");
-    setInterval(() => {
-      let now = new Date();
-      let timeout = new Date(timeElement.data("time"));
-      let timeRemaining = (+timeout - +now) / 1000;
-      let minutesRemaining = Math.floor(timeRemaining / 60);
-
-      outputElement.text(minutesRemaining + " minuter");
-    }, 1000);
+  $("[data-fn-session-timeout]").each((_, elm) => {
+    const $elm = $(elm);
+    let outputElement = $elm.find("[data-fn-session-timeout-output]");
+    let timeElement = $elm.find("time");
+    let timeout = new Date(timeElement.data("time"));
+    function update() {
+      const minutesRemaining = minutesUntil(timeout);
+      outputElement.text(`(${minutesRemaining} minuter kvar)`);
+    }
+    update();
+    setInterval(update, 10 * 1000);
   });
+}
+
+function minutesUntil(until: Date) {
+  let now = new Date();
+  let timeRemaining = (+until - +now) / 1000;
+  return Math.max(0, Math.floor(timeRemaining / 60));
 }
