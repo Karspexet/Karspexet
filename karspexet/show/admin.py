bfc9448c4d61e6302808bc01f860e79eb0064518
Dan Strokirk <dan.strokirk@gmail.com>
2022-06-08 04:26:42 +0200

    Add better ShowForm

--- karspexet/show/admin.py
+++ karspexet/show/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.utils import timezone
 
@@ -9,6 +10,25 @@ class ProductionAdmin(admin.ModelAdmin):
     list_display = ("name", "alt_name")
 
 
+class ShowForm(forms.ModelForm):
+    class Meta:
+        model = Show
+        fields = [
+            "production",
+            "venue",
+            "date",
+            "free_seating",
+            "visible",
+            "short_description",
+        ]
+
+    visible = forms.BooleanField(initial=False, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["production"].initial = Production.objects.last()
+
+
 @admin.register(Show)
 class ShowAdmin(admin.ModelAdmin):
     list_display = ("date_string", "production", "venue", "visible", "is_upcoming")
@@ -16,6 +36,7 @@ class ShowAdmin(admin.ModelAdmin):
     list_filter = ("visible", "production")
     exclude = ("slug",)
     ordering = ("-pk",)
+    form = ShowForm
 
     @admin.display(boolean=True)
     def is_upcoming(self, obj):
