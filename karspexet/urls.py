395b6f8d67806db642a9a461ec61d00260d5e24d
Martin Frost <frost@ceri.se>
2023-12-23 22:14:16 +0100

    Provide favicon.ico where everyone expects it
    ...and redirect to where it actually lives.

--- karspexet/urls.py
+++ karspexet/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("500/", TemplateView.as_view(template_name="500.html")),
@@ -10,6 +11,7 @@ urlpatterns = [
     path("economy/", include("karspexet.economy.urls")),
     path("venue/", include("karspexet.venue.urls")),
     path("ticket/", include("karspexet.ticket.urls")),
+    re_path(r"^favicon\.ico$", RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
     # Important to keep this last
     path("", include("cms.urls")),
 ]
